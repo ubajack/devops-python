7bb9126296e89d8b83e44da727a6f6d4d66529ac
ubajack <jacques.bach@ubalia.fr>
2023-07-19 14:40:33 +0200

    Add some prints

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 class Tom: 
     name: str = 'Tom'   # Tom's name is Tom
     money: float = 0.0  # Tom has no money
+    
+    def __str__(self):
+        return f'Tom is called {self.name} and he has ${self.money}'
+
+    def __repr__(self):
+        return f'Tom(\'{self.name}\', {self.money})'
+
+if __name__ == '__main__':
+    tom = Tom()
+    print(f'{repr(tom)} was created!')
+    print(str(tom))
